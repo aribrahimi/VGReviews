07a6d70a9d2977f719a88f0f52facbc65238218d
aribrahimi <ar.ibrahimy@gmail.com>
2021-02-26 23:47:55 -0800

    create base.html and frontpage.html

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def hello(request):
-    return HttpResponse('Hello World!!!!!')
+def frontpage(request):
+    return render(request,'blog/frontpage.html')
