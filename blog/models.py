edfe707742797bb64f8dd1e3fe03917b5b0e24cc
aribrahimi <ar.ibrahimy@gmail.com>
2021-02-27 11:27:35 -0800

    comment form

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     class Meta:
         ordering = ['-date_added']
-class Commit(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post,related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=225)
     email = models.EmailField()
