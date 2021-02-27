9a14bf6203e838d74d092991fef05c7dc6114049
aribrahimi <ar.ibrahimy@gmail.com>
2021-02-27 01:36:03 -0800

    show post detail

--- VGReviews/urls.py
+++ VGReviews/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import frontpage
+from blog.views import frontpage, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',frontpage, name='frontpage'),
+    path('<slug:slug>/', post_detail, name='post_detail'),
 
 ]
