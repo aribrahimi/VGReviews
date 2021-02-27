342625ce02ee7796221aaf94733a6772e5ea6aaa
aribrahimi <ar.ibrahimy@gmail.com>
2021-02-27 01:48:50 -0800

    comment section

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,14 @@ class Post(models.Model):
     intro = models.TextField()
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-
     class Meta:
         ordering = ['-date_added']
+class Commit(models.Model):
+    post = models.ForeignKey(Post,related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    email = models.EmailField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
